8306ada4113d7c3541ece879974e51e7b93b7ebb
Taranjeet Singh Dhingra <taranjeetsingh257@gmail.com>
2023-01-09 18:50:20 -0500

    configured routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import { FooterComponent } from './components/FooterComponent';
 import { HeaderComponent } from './components/HeaderComponent';
 import { ListEmployeeComponent } from './components/ListEmployeeComponent';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />
+
+      <BrowserRouter>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<ListEmployeeComponent />}/>
+            <Route path="/employees" element={<ListEmployeeComponent />} />
+          </Routes>
+        </div>
       <FooterComponent />
+      </BrowserRouter>
     </div>
   );
 }
