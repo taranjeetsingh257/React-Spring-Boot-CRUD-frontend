bf351724d890b36098871e530870282531708670
Taranjeet Singh Dhingra <taranjeetsingh257@gmail.com>
2023-01-09 17:33:42 -0500

    bootstrap navbar

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Employee Management App</a>
+            </div>
+        </nav>
+    </div>
+  )
+}
