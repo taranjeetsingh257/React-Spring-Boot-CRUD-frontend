c2859acabf4dc4ce2273205b4da7d14a5624b610
Taranjeet Singh Dhingra <86784739+taranjeetsingh257@users.noreply.github.com>
2023-01-29 16:07:59 -0500

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const HeaderComponent = () => {
   return (
-    <div>
+    <div className='my-header'>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Employee Management App</a>
+                <a className="navbar-brand mx-auto" href="/">Employee Management App</a>
             </div>
         </nav>
     </div>
