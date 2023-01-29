7818837d49e4174df91dd9406830ed57aebff970
Taranjeet Singh Dhingra <86784739+taranjeetsingh257@users.noreply.github.com>
2023-01-29 16:07:46 -0500

    Update FooterComponent.js

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,8 +3,13 @@ import React from 'react'
 export const FooterComponent = () => {
   return (
     <div>
-        <footer className = "footer">
-            <span className="text-muted">All Rights Reserved 2023 @Taranjeet</span>
+        <footer className = "my-footer container-fluid" style={{"padding": "10px"}}>
+            <span>App Made by: 
+              <strong>
+                <a href='https://www.linkedin.com/in/taranjeet-dhingra-160943192/'  className="blink">Taranjeet
+                </a>
+              </strong> 
+            </span>
         </footer>
     </div>
   )
